a4aa866b51e2f7ef6fb3751b874f7f526f7a2c75
= <mypochtav@gmail.com>
2021-12-27 20:39:41 +0200

    add bench test

---
+++ cmd/bench_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math"
+	"strings"
+	"testing"
+)
+
+func ExtendSplitCommand(times int) {
+    for i := 0; i < times; i++ {
+        input,_  := ioutil.ReadFile("./example/example-test.txt")
+        parts := strings.Fields(string(input))
+        cmd := parts[0]
+        params := parts[1]
+        delimiter := parts[2]
+        params = fmt.Sprintf("%s-%s", params, params)
+        newCommand := fmt.Sprintf("%s %s %s", cmd, params, delimiter)
+        ioutil.WriteFile("./example/example-test.txt", []byte(newCommand), 0644)
+    }
+}
+
+func SetupFile() {
+    ioutil.WriteFile("./example/example-test.txt", []byte("split uno-dos-tres-cuatro-cinco -"), 0644)
+}
+
+func BenchmarkParser(b *testing.B) {
+    for i := 1; i <= 5; i++ {
+        SetupFile()
+        ExtendSplitCommand(i)
+        command,_  := ioutil.ReadFile("./example/example-test.txt")
+        b.Run(fmt.Sprintf("%d elements", int(math.Pow(2, float64(i)))), func(b *testing.B) {
+            parse(string(command))
+        })
+    }
+}
